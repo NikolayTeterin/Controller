2c8af7eee2ade6671ec20261e51e7f60b71c9d18
Pavel <PavelDanilov.76@yandex.ru>
2017-03-05 04:06:29 +0300

    132

--- Controller.ino
+++ Controller.ino
@@ -1,87 +1,117 @@
-#define swPin 5 //пин включения приемника
-#define rxPin 2 //пин приемника
-
-void grab();
+#define txPin 3 //пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+#define button 13 //пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
 
 void setup()
 {
-	pinMode(rxPin, INPUT);
-	// всегда устанавливаем пин включения приемника в 0 и не трогаем его больше
-	pinMode(swPin, OUTPUT);
-	digitalWrite(swPin, LOW);
+	pinMode(txPin, OUTPUT);
+	pinMode(button, INPUT);
 
-	Serial.begin(9600);  // тут поставьте свою скорость
+	Serial.begin(9600);  // пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 	Serial.println("Came started");
-
-	attachInterrupt(0, grab, CHANGE); // обратите внимание на первый параметр - если у вас другая ардуина, он может быть не 1, а 0!
-	interrupts();
-
 }
 
-volatile static long code = 0;
-long lastCode = 0;
-int i, j;
+volatile static long lastCode = 0;
+int i = 0, j = 0;
 long variant;
 long someCode = 1234;
 void SendCame4(long Code);
 
-void loop()
-{
+//Brut
+unsigned int startTime;
+long BrutCode = 0;
+void SendCameBitBrut(byte bBrut);
+void SendCameBrut(long CodeBrut);
 
-}
+void loop() {
 
-// ВЕСЬ КОД НИЖЕ - ПРИЕМ
-
-#define MAX_DELTA 200
-// длительность импульсов может плавать в зависимости от заряда батареии - макс допуск 200 мкс
-boolean CheckValue(unsigned int base, unsigned int value)
-{
-	return ((value == base) || ((value > base) && ((value - base) < MAX_DELTA)) || ((value < base) && ((base - value) < MAX_DELTA)));
-}
-volatile unsigned long prevtime;
-volatile unsigned int lolen, hilen, state;
-volatile static byte cameCounter = 0;    // count of bits stored
-volatile static long cameCode = 0;       // code itself
-
-void grab()
-{
-	state = digitalRead(rxPin);
-	if (state == HIGH)
-		lolen = micros() - prevtime;
-	else
-		hilen = micros() - prevtime;
-	prevtime = micros();
-
-	if (state == LOW)
+	if (digitalRead(button) == HIGH)
 	{
-		// последовательность закончилась
-		if (CheckValue(320, hilen) && CheckValue(640, lolen)) // valid 1
+		delay(500);
+		if (digitalRead(button) == LOW)
 		{
-			cameCode = (cameCode << 1) | 1;
-			cameCounter++;
+			SendCame4(someCode);
+			Serial.println();
+			Serial.print(someCode);
+			Serial.println(" was transmitted");
+			return;
 		}
-		else if (CheckValue(640, hilen) && CheckValue(320, lolen)) // valid 0
+		/*else
 		{
-			cameCode = (cameCode << 1) | 0;
-			cameCounter++;
-		}
-		else cameCounter = 0;
+			delay(995);
+			if (digitalRead(button) == LOW)
+			{
+				do
+				{
+					if (digitalRead(button) == HIGH)
+						return;
+
+					startTime = micros();
+					SendCameBrut(BrutCode);
+					BrutCode++;
+					if (BrutCode == 4095)
+					{
+						BrutCode = 0;
+						break;
+					}
+				} while (digitalRead(button) == LOW);
+			}
+		}*/
 	}
-	else
-		if (lolen > 1000 &&
-			(cameCounter == 12 || cameCounter == 13) &&
-			((cameCode & 0xfff) != 0xfff))
-		{
-			code = cameCode & 0xfff;
-			cameCounter = 0;
-			cameCode = 0;
+}
+
+
+// пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅ
+void SendCame4(long Code) {
+	for (int j = 0; j < 4; j++) { // пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ 4 пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ.
+								  // пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ 
+		digitalWrite(txPin, HIGH);
+		delayMicroseconds(320);
+		digitalWrite(txPin, LOW);
+		for (int i = 12; i > 0; i--) {
+			byte b = bitRead(Code, i - 1); // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ
+			if (b) {
+				digitalWrite(txPin, LOW); // 1
+				delayMicroseconds(640);
+				digitalWrite(txPin, HIGH);
+				delayMicroseconds(320);
+			}
+			else {
+				digitalWrite(txPin, LOW); // 0
+				delayMicroseconds(320);
+				digitalWrite(txPin, HIGH);
+				delayMicroseconds(640);
+			}
 		}
-	if (code != 0)
+		digitalWrite(txPin, LOW);
+		delayMicroseconds(11520);
+	}
+}
+
+void SendCameBitBrut(byte bBrut)
+{
+	delayMicroseconds(320);
+	if (!bBrut) digitalWrite(txPin, HIGH);
+	delayMicroseconds(320);
+	digitalWrite(txPin, HIGH);
+	delayMicroseconds(320);
+	digitalWrite(txPin, LOW);
+}
+
+void SendCameBrut(long CodeBrut)
+{
+	Serial.println();
+	Serial.print("***BRUT***        ");
+	Serial.print(CodeBrut);
+	for (int j = 0; j < 4; j++) // пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅ - 4 пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ.
 	{
-		if (code == lastCode)
-			Serial.print("*");
-		Serial.println(code);
-		lastCode = code;
-		code = 0;
+		digitalWrite(txPin, HIGH);
+		delayMicroseconds(320);
+		digitalWrite(txPin, LOW);// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+
+		for (byte i = 12; i>0; i--)
+		{
+			SendCameBitBrut(bitRead(CodeBrut, i - 1)); // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ
+		}
+		delay(16);
 	}
 }
